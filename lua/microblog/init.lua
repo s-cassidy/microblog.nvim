f24fa36b171bc307c3c2d28ec6880785eb9f3be4
Sam Cassidy <sam@cassidys.email>
2024-04-30 10:53:45 +0100

    Improve getting of API key

--- lua/microblog/init.lua
+++ lua/microblog/init.lua
@@ -4,21 +4,13 @@ local get = require("microblog.get")
 local post = require("microblog.post")
 local async = require("plenary.async.async")
 
-M = {}
-M.pick_post = get.pick_post
-M.push_post = post.push_post
-
 local M = {}
 M.pick_post = get.pick_post
 M.push_post = post.push_post
-local function get_api_key()
-  return os.getenv(config.api_key_variable)
-end
 
 function M.setup(opts)
   config.api_key_variable = config.api_key_variable or opts.api_key_variable
   config.blogs = opts.blogs
-  config.api_key = get_api_key()
 end
 
 return M
