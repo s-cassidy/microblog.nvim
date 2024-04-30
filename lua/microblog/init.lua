be569544be428a587b454debae29a7c9ec944023
Sam Cassidy <sam@cassidys.email>
2024-04-30 13:16:08 +0100

    Add some user commands

--- lua/microblog/init.lua
+++ lua/microblog/init.lua
@@ -7,6 +7,12 @@ local M = {}
 M.pick_post = get.pick_post
 M.push_post = post.push_post
 M.display_post_status = status.display_post_status
+M.reset_post_status = status.reset_post_status
+
+vim.api.nvim_create_user_command("MicroBlogPickPost", get.pick_post, {})
+vim.api.nvim_create_user_command("MicroBlogPushPost", post.push_post, {})
+vim.api.nvim_create_user_command("MicroBlogDisplayStatus", status.display_post_status, {})
+vim.api.nvim_create_user_command("MicroBlogResetStatus", status.reset_post_status, {})
 
 local function get_api_key()
   return os.getenv(config.api_key_variable)
