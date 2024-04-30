9870d418f0ce40e553975895526ecd55a9e13240
Sam Cassidy <sam@cassidys.email>
2024-04-30 11:29:39 +0100

    Revert change to getting API key

--- lua/microblog/config.lua
+++ lua/microblog/config.lua
@@ -1,28 +1,5 @@
-local config = {
+return {
   api_key_variable = "MB_API_KEY",
   blogs = {
   }
 }
-
-local function read_api_key()
-  return os.getenv(config.api_key_variable)
-end
-
-local mt = {}
-
-setmetatable(config, mt)
-
-mt.__index = function(table, key)
-  if key == "api_key" then
-    local api_key = read_api_key()
-    if #api_key > 0 then
-      return api_key
-    else
-      return nil
-    end
-  else
-    return table[key]
-  end
-end
-
-return config
--- lua/microblog/init.lua
+++ lua/microblog/init.lua
@@ -1,16 +1,21 @@
 local config = require("microblog.config")
-local categories = require("microblog.categories")
 local get = require("microblog.get")
 local post = require("microblog.post")
-local async = require("plenary.async.async")
+local status = require("microblog.status")
 
 local M = {}
 M.pick_post = get.pick_post
 M.push_post = post.push_post
+M.display_post_status = status.display_post_status
+
+local function get_api_key()
+  return os.getenv(config.api_key_variable)
+end
 
 function M.setup(opts)
   config.api_key_variable = config.api_key_variable or opts.api_key_variable
   config.blogs = opts.blogs
+  config.api_key = get_api_key()
 end
 
 return M
